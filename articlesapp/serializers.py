b64e2d648ad84f247e4ecda47c79f3a420f3547c
nazgual <nazgual@ukr.net>
2020-05-25 18:44:21 +0300

    Changed SQLite to MySQL

--- articlesapp/serializers.py
+++ articlesapp/serializers.py
@@ -19,3 +19,15 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ("id", "username", "first_name", "last_name", "email", "is_active")
+
+
+
+# {
+#     "article": {
+#         "id": 3,
+#         "title": "Конференция для тестировщиков #1 в Украине — QA Fest",
+#         "text": "Уже на этой неделе не пропусти конференцию для тестировщиков.",
+#         "date": "2018-09-18T18:00:00Z",
+#         "author": 1
+#     }
+# }
--- articleswebsite/settings.py
+++ articleswebsite/settings.py
@@ -75,10 +75,22 @@ WSGI_APPLICATION = 'articleswebsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_articles',
+        'USER': 'djangouser',
+        'PASSWORD': 'djangouser_password',
+        'HOST': '192.168.1.32',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
+        'OPTIONS': {'charset': 'utf8mb4'},
     }
 }
 
