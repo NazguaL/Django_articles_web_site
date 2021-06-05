f82cdd20ff2dc8a5e8ed1df8ceb694487188f104
nazgual <nazgual@ukr.net>
2021-06-05 21:14:26 +0300

    Revert to SQLite

--- articlesapp/tests.py
+++ articlesapp/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+# {
+#     "article": {
+#         "id": 3,
+#         "title": "Конференция для тестировщиков #1 в Украине — QA Fest",
+#         "text": "Уже на этой неделе не пропусти конференцию для тестировщиков.",
+#         "date": "2018-09-18T18:00:00Z",
+#         "author": 1
+#     }
+# }
